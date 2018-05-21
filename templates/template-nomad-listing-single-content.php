3d2a30d984fdd8e6e81fdaa760c40677af183d2d
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-05-21 20:13:32 +0300

    Code cleanup

--- templates/template-nomad-listing-single-content.php
+++ templates/template-nomad-listing-single-content.php
@@ -1,5 +1,17 @@
 <?php
-// Start the Loop.
+/**
+ * Single Listing Template
+ *
+ * Template file for single nomad listings.
+ *
+ * @category Theme
+ * @package  Nomad_Magazine_Theme
+ * @author   Kelvin Jayanoris <kelvin@jayanoris.com>
+ * @license  https://github.com/moshthepitt/nomad-magazine-theme/blob/master/LICENSE Apache 2.0
+ * @link     https://github.com/moshthepitt/nomad-magazine-theme
+ */
+
+$nomad_single_review = 1;
 ?>
 <div class="sidebar_wrapper">
     <div class="sidebar_top"></div>
@@ -15,44 +27,42 @@
             </div>
             <div class="single_tour_booking_wrapper themeborder external">
                 <div class="single_tour_booking_external_wrapper">
-                    Click button below to begin booking&nbsp;Pro Tour
-                    <a href="http://example.com" class="button" target="_blank">Book This Tour</a>
+                    Some text goes here
+                    <a href="http://example.com" class="button" target="_blank">
+                        Book This Tour
+                    </a>
                 </div>
             </div>
 
-            <?php if ( is_active_sidebar( 'single-nomad-listings-sidebar' ) ) : ?>
+            <?php if (is_active_sidebar('single-nomad-listings-sidebar')) : ?>
                 <br/>
                 <ul class="sidebar_widget">
-                    <?php dynamic_sidebar( 'single-nomad-listings-sidebar' ); ?>
+                    <?php dynamic_sidebar('single-nomad-listings-sidebar');?>
                 </ul>
-            <?php endif; ?>
+            <?php endif;?>
         </div>
-        
     </div>
     <br class="clear" />
     <div class="sidebar_bottom"></div>
 </div>
-<?php if ( have_posts() ) : ?>
-    <?php while ( have_posts() ) : ?>
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : ?>
         <?php the_post(); ?>
         <div class="sidebar_content ">
-            <h1><?php the_title();?></h1>
+            <h1><?php the_title(); ?></h1>
             <br class="clear" />
             <div class="single_tour_content">
-                <?php the_content();?>
+                <?php the_content(); ?>
             </div>
             <?php
-                //Check if enable tour review
-                $nomad_single_review = 1;
-
-                //Display tour comment
-                if (comments_open($post->ID) && !empty($nomad_single_review)) {
-                    ?>
-                        <div class="fullwidth_comment_wrapper sidebar">
-                            <?php comments_template('', true);?>
-                        </div>
-                    <?php
-                }
+            //Display listing comments
+            if (comments_open($post->ID) && !empty($nomad_single_review)) {
+            ?>
+                <div class="fullwidth_comment_wrapper sidebar">
+                    <?php comments_template('', true); ?>
+                </div>
+            <?php
+            }
             ?>
         </div>
     <?php endwhile;?>
